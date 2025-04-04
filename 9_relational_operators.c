289df1b5c0c16e2b518eae2c9e0903d2a60f8960
maitripandit25 <maitripandit25@gmail.com>
2025-04-04 19:07:42 +0530

    Add files via upload

---
+++ 9_relational_operators.c
@@ -0,0 +1,25 @@
+//concept of relational operators
+#include<stdio.h>
+void main()
+{
+    int a = 10 , b = 10 , result;
+    result = a==b;
+    printf("\n %d = %d == %d",result,a,b);
+
+    result = a!=b;
+    printf("\n %d = %d != %d",result,a,b);
+
+    result = a < b;
+    printf("\n %d = %d < %d",result,a,b);
+
+    result = a > b;
+    printf("\n %d = %d > %d",result,a,b);
+
+    result = a <= b;
+    printf("\n %d = %d <= %d",result,a,b);
+
+    result = a >= b; result = a==b;
+    printf("\n %d = %d >= %d",result,a,b);
+
+}   
+    
