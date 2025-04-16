59743966926f41fc236d9ec2db5435dff418f7a8
maitripandit25 <maitripandit25@gmail.com>
2025-04-16 09:55:20 +0530

    Add files via upload

---
+++ 22_nested_1.c
@@ -0,0 +1,37 @@
+/* write a program to display 24 hours format and display it in
+12 hours format
+input = 16 hours output = 4 PM
+steps
+  1.create variable hours
+  2.accpet variable from user in hours variable
+  3.check if hours are valid then
+    3.2 check if hours is above 11 then substract 12 from hours
+    and display hours with PM word
+    3.3 otherwise
+     display hours with AM word   
+  4. otherwise display invalid hours
+*/
+#include<stdio.h>
+void main()
+{
+    int hours;
+    printf("\n enter hours in 24 hours format");
+    scanf("%d",&hours);
+    if(hours>=1 && hours<=24 )
+    {
+        if(hours>11)
+       {
+        hours = hours - 12;
+        printf("%d PM",hours);
+       } 
+        else
+       {
+        printf("%d AM",hours);
+       }
+    }  
+    else
+    {
+        printf("invalid hours");
+    }
+    printf("\n GOOD BYE.");
+}
