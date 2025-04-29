24694030bee40252c6b54f02f938450246b08bf3
maitripandit25 <maitripandit25@gmail.com>
2025-04-29 12:05:44 +0530

    Add files via upload

---
+++ 32_nested_loop_1.c
@@ -0,0 +1,27 @@
+""""/*
+   write a program to print following pattern
+   *
+   * *
+   * * *
+   * * * *
+   * * * * *
+*/
+#include<stdio.h>
+void main()
+{
+    int column,row;
+    
+    for(row=1;row<=5;row++)
+    {
+       for(column = 1; column<=row; column++)
+       {
+        printf("*");
+       }
+       printf("\n");
+
+    }
+    
+   
+
+    
+}
