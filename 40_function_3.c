33ec1364dd278746a54521d0c974a44fad4f668a
maitripandit25 <maitripandit25@gmail.com>
2025-05-02 09:52:25 +0530

    Add files via upload

---
+++ 40_function_3.c
@@ -0,0 +1,22 @@
+/* write a function make addition of 2 numberss.*/
+#include<stdio.h>
+int add(int a,int b)
+{
+    int ans;
+    ans = a + b;
+    return ans;
+}
+void main()
+{
+    int n;
+    n = add(5,10);
+    printf("%d",n);
+
+    
+   
+}
+/*int a,b;
+    printf("enter value for a");
+    scanf("%d",&a);
+    printf("enter value for b");
+    scanf("%d",&b);*/
