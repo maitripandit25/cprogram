fe0db4b1845d5978a4b647b8b1a6b90aa5ee80c5
maitripandit25 <maitripandit25@gmail.com>
2025-05-02 09:51:41 +0530

    Add files via upload

---
+++ 38_function_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printline()
+{
+    printf("\n--------------------\n");
+}
+
+void main()
+{
+    printline();
+    printline();
+    printf("c language");
+    printline();
+    printf("hello world");
+    printline();
+    printline();
+}
